6ea5ec30184d90db30013049e8fd62729c9b9c1b
Florian Echtler <floe@butterbrot.org>
2016-05-19 16:34:38 +0200

    summary commit

--- find-links.sh
+++ find-links.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-egrep 'https?://' *.txt | egrep -v 'youtube.com|books.google.com|doi.org|((doi|dl).acm.org)' 
+egrep 'https?://' raw-text/*.txt | egrep -v 'youtube.com|books.google.com|doi.org|((doi|dl).acm.org)' 
