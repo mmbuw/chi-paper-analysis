5eba8ad0d303a1dc049d27e786b6f88fbae079c9
Florian Echtler <floe@butterbrot.org>
2017-01-05 18:52:09 +0100

    done!

--- find-open-source-code.sh
+++ find-open-source-code.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 egrep -i '(open(-| )?source)|(source(-| )?code)' raw-text/*.txt | cut -d: -f1 | sort -u
+#egrep -l -z -i '(open(-|[[:space:]])?source)|(source(-|[[:space:]])?code)' raw-text/*.txt | cut -d: -f1 | sort -u
