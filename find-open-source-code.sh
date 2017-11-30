c9a857b58b19fa340bc40f2ddf0792ad21b3ab0e
Florian Echtler <floe@butterbrot.org>
2017-11-30 14:40:53 +0100

    add raw 2017 data

--- find-open-source-code.sh
+++ find-open-source-code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-egrep -i '(open(-| )?source)|(source(-| )?code)' raw-text/*.txt | cut -d: -f1 | sort -u
+egrep -i '(open(-| )?source)|(source(-| )?code)|supplement' raw-text/*.txt | cut -d: -f1 | sort -u
 #egrep -l -z -i '(open(-|[[:space:]])?source)|(source(-|[[:space:]])?code)' raw-text/*.txt | cut -d: -f1 | sort -u
