f0aa33dc0bbfd42780c8592316e26042b4304ea5
Florian Echtler <floe@butterbrot.org>
2017-11-30 17:12:39 +0100

    all candidates that need looking at paper

--- find-open-source-code.sh
+++ find-open-source-code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-egrep -i '(open(-| )?source)|(source(-| )?code)|supplement' raw-text/*.txt | cut -d: -f1 | sort -u
+egrep -C1 -i '(open(-| )?source)|(source(-| )?code)|supplement' raw-text/*.txt | cut -d: -f1 | sort -u
 #egrep -l -z -i '(open(-|[[:space:]])?source)|(source(-|[[:space:]])?code)' raw-text/*.txt | cut -d: -f1 | sort -u
